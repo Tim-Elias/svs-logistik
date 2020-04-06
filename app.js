90a788cb1158a4a5f6b8f66db3a1254cf14486f0
Илья Тимофеев <elias@MacBook-Pro-Ila.local>
2020-04-06 13:53:36 +0700

    no

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use('/disp', require('./routes/disp'));
 app.use('/requestdelivery', require('./routes/requestdelivery'));
 app.use('/mydisp', require('./routes/mydisp'));
 app.use('/pdfdisp', require('./routes/pdfdisp'));
-app.use('/getscan', require('./routes/getscan'));
+app.use('/getscan', require('./routes/getscan')); 
 app.use('/avia', require('./routes/avia'));
 app.use('/cargo', require('./routes/cargo'));
 app.use('/term', require('./routes/term'));
